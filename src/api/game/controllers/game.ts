367db28abb09140e01702a175484c86676dcb247
Samir <samir.andrade@ceuma.br>
2023-10-02 20:11:47 -0300

    wip: chamada a api do gog

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -1,7 +1,11 @@
-/**
- * game controller
- */
-
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController('api::game.game',({strapi})=>({
+  async populate(ctx) {
+    console.log("RODANDO NO SERVER");
+    await strapi.service("api::game.game").populate(ctx.query);
+    ctx.send("FINALIZADO NO CLIENT");
+  },
+}));
+
+
--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -3,5 +3,16 @@
  */
 
 import { factories } from '@strapi/strapi';
+import axios from 'axios'
 
-export default factories.createCoreService('api::game.game');
+export default factories.createCoreService('api::game.game',()=>({
+  async populate(params){
+    const gogApiUrl=`https://www.gog.com/games/ajax/filtered?mediaType=game`;
+
+    const {
+      data:{products},
+    } = await axios.get(gogApiUrl);
+    console.log(products);
+  }
+
+}));
