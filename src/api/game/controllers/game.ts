89f58f9146be2eb33bc037c1864f2daa7904bbfb
Samir <samir.andrade@ceuma.br>
2023-10-11 17:54:12 -0300

    feat: criando scrum para pegar cursos

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,9 +2,16 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::game.game',({strapi})=>({
   async populate(ctx) {
-    console.log("RODANDO NO SERVER");
-    await strapi.service("api::game.game").populate(ctx.query);
-    ctx.send("FINALIZADO NO CLIENT");
+    const options = {
+      limit: 48,
+      order: "desc:trending",
+      ...ctx.query,
+    };
+
+    await strapi.service("api::game.game").populate(options);
+
+
+    ctx.send("Finished populating games!")
   },
 }));
 
